5e7c8fe26dbff92589ebbccd1fe272ed44d4f5e6
星笺 <1600785006@qq.com>
2026-03-09 20:59:24 +0800

    Update build configuration and libraries
    Remove outdated PDF and CMake system file; adjust CMakeLists and regenerate build metadata. CMake minimum version changed (3.27 -> 3.20), CMakeCache and configure log were rewritten (generator/compilers/flags/paths updated, Ninja used), and many build artifacts (.exe/.ilk/.pdb) were refreshed. Also apply modifications to PolynomialList.cpp and PolynomialMap.cpp (library/source updates) to match the rebuilt artifacts.

--- Homeworks/0_cpp_warmup/project/src/libraries/shared/PolynomialList.cpp
+++ Homeworks/0_cpp_warmup/project/src/libraries/shared/PolynomialList.cpp
@@ -1,69 +1,192 @@
 #include "PolynomialList.h"
+#include <list>
+#include <fstream>
+#include <iostream>
+#include <cmath>
 
 using namespace std;
 
 PolynomialList::PolynomialList(const PolynomialList& other) {
-    // TODO
+    // Special: empty
+    if (other.m_Polynomial.empty()) {
+        return;
+    }
+
+    for (const auto& term : other.m_Polynomial) {
+        m_Polynomial.push_back(term);
+    }
+
 }
 
 PolynomialList::PolynomialList(const string& file) {
-    // TODO
+    ReadFromFile(file);
+    return;
 }
 
 PolynomialList::PolynomialList(const double* cof, const int* deg, int n) {
-    // TODO
+    // Special: negative n
+    if (n < 0) {
+        cout << "Negative n: " << n << endl;
+        return;
+    }
+    // Special: negative degree
+    for (int i = 0; i < n; ++i) {
+        if (deg[i] < 0) {
+            cout << "Negative degree: " << deg[i] << endl;
+            return;
+        }
+    }
+    for (int i = 0; i < n; ++i) {
+        AddOneTerm(Term(deg[i], cof[i]));
+    }
+    return;
 }
 
 PolynomialList::PolynomialList(const vector<int>& deg, const vector<double>& cof) {
-    // TODO
+    // Special: negative degree
+    for (size_t i = 0; i < deg.size(); ++i) {
+        if (deg[i] < 0) {
+            cout << "Negative degree: " << deg[i] << endl;
+            return;
+        }
+    }
+    for (size_t i = 0; i < deg.size(); ++i) {
+        AddOneTerm(Term(deg[i], cof[i]));
+    }
+    return;
 }
 
 double PolynomialList::coff(int i) const {
-    // TODO
-    return 0.; // you should return a correct value
+    for (const auto& term : m_Polynomial) {
+        if (term.deg == i) {
+            return term.cof;
+        }
+    }
+    return 0.f; // default value
 }
 
 double& PolynomialList::coff(int i) {
-    // TODO
-    static double ERROR; // you should delete this line
-    return ERROR; // you should return a correct value
+    for (auto& term : m_Polynomial) {
+        if (term.deg == i) {
+            return term.cof;
+        }
+    }
+    // If not found, add a new term with degree i and coefficient 0
+    return AddOneTerm(Term(i, 0)).cof;
 }
 
 void PolynomialList::compress() {
-    // TODO
+    for (auto it = m_Polynomial.begin(); it != m_Polynomial.end();) {
+        if (std::abs(it->cof) < 1e-8) {
+            it = m_Polynomial.erase(it);
+        } else {
+            ++it;
+        }
+    }
 }
 
 PolynomialList PolynomialList::operator+(const PolynomialList& right) const {
-    // TODO
-    return {}; // you should return a correct value
+    // Special: empty
+    if (right.m_Polynomial.empty()) {
+        return *this;
+    }
+    PolynomialList result(*this);
+    for (const auto& term : right.m_Polynomial) {
+        result.AddOneTerm(term);
+    }
+    result.compress();
+    return result;
 }
 
 PolynomialList PolynomialList::operator-(const PolynomialList& right) const {
-    // TODO
-    return {}; // you should return a correct value
+    // Special: empty
+    if (right.m_Polynomial.empty()) {
+        return *this;
+    }
+
+    PolynomialList result(*this);
+    for (const auto& term : right.m_Polynomial) {
+        result.AddOneTerm(Term(term.deg, -term.cof));
+    }
+    result.compress();
+    return result;
 }
 
 PolynomialList PolynomialList::operator*(const PolynomialList& right) const {
-    // TODO
-    return {}; // you should return a correct value
+    // Special: empty
+    if (right.m_Polynomial.empty() || m_Polynomial.empty()) {
+        return PolynomialList();
+    }
+
+    PolynomialList result;
+    for (const auto& term1 : m_Polynomial) {
+        for (const auto& term2 : right.m_Polynomial) {
+            result.AddOneTerm(Term(term1.deg + term2.deg, term1.cof * term2.cof));
+        }
+    }
+    result.compress();
+    return result;
 }
 
 PolynomialList& PolynomialList::operator=(const PolynomialList& right) {
-    // TODO
+    if (this == &right) {
+        return *this;
+    }
+    m_Polynomial.clear();
+    for (const auto& term : right.m_Polynomial) {
+        m_Polynomial.push_back(term);
+    }
     return *this;
 }
 
 void PolynomialList::Print() const {
-    // TODO
+    // Special: empty
+    if (m_Polynomial.empty()) {
+        cout << "0" << endl;
+        return;
+    }
+    for (const auto& term : m_Polynomial) {
+        cout << term.cof << "x^" << term.deg << " ";
+    }
+    cout << endl;
 }
 
 bool PolynomialList::ReadFromFile(const string& file) {
-    // TODO
-    return false; // you should return a correct value
+    m_Polynomial.clear();
+    ifstream fin(file);
+    if (!fin.is_open()) {
+        cout << "Failed to open file: " << file << endl;
+        return false;
+    }
+
+    int deg;
+    double cof;
+    int num = 0;
+    char check;
+    if (!(fin >> check >> num)) {
+        cout << "Empty file: " << file << endl;
+        return false;
+    }
+
+    for (int i = 0; i < num; ++i) {
+        fin >> deg >> cof;
+        AddOneTerm(Term(deg, cof));
+    }
+
+    return true; 
 }
 
 PolynomialList::Term& PolynomialList::AddOneTerm(const Term& term) {
-    // TODO
-    static Term ERROR; // you should delete this line
-    return ERROR; // you should return a correct value
+    for (auto& t = m_Polynomial.begin(); t != m_Polynomial.end(); t++) {
+        if (t->deg == term.deg) {
+            t->cof += term.cof;
+            return *t;
+        }
+        if (t->deg < term.deg) {
+            m_Polynomial.insert(t, term);
+            return *prev(t);
+        }
+    }
+    m_Polynomial.push_back(term);
+    return m_Polynomial.back();
 }
--- Homeworks/0_cpp_warmup/project/src/libraries/static/PolynomialMap.cpp
+++ Homeworks/0_cpp_warmup/project/src/libraries/static/PolynomialMap.cpp
@@ -8,63 +8,142 @@
 using namespace std;
 
 PolynomialMap::PolynomialMap(const PolynomialMap& other) {
-    // TODO
+	// Special: empty
+	if (other.m_Polynomial.empty()) {
+		return;
+	}
+
+	for (const auto& term : other.m_Polynomial) {
+		m_Polynomial[term.first] = term.second;
+	}
 }
 
 PolynomialMap::PolynomialMap(const string& file) {
     ReadFromFile(file);
+	return;
 }
 
 PolynomialMap::PolynomialMap(const double* cof, const int* deg, int n) {
-	// TODO
+	// Special: negative n
+	if (n < 0) {
+		cout << "Negative n: " << n << endl;
+		return;
+	}
+	// Special: negative degree
+	for (int i = 0; i < n; ++i) {
+		if (deg[i] < 0) {
+			cout << "Negative degree: " << deg[i] << endl;
+			return;
+		}
+	}
+	for (int i = 0; i < n; ++i) {
+		m_Polynomial[deg[i]] += cof[i];
+	}
 }
 
 PolynomialMap::PolynomialMap(const vector<int>& deg, const vector<double>& cof) {
 	assert(deg.size() == cof.size());
-	// TODO
+	// Special: negative degree
+	for (size_t i = 0; i < deg.size(); ++i) {
+		if (deg[i] < 0) {
+			cout << "Negative degree: " << deg[i] << endl;
+			return;
+		}
+	}
+	for (size_t i = 0; i < deg.size(); ++i) {
+		m_Polynomial[deg[i]] += cof[i];
+	}
 }
 
 double PolynomialMap::coff(int i) const {
-	// TODO
-	return 0.f; // you should return a correct value
+	auto it = m_Polynomial.find(i);
+	if (it != m_Polynomial.end()) {
+		return it->second;
+	}
+	return 0.f;
 }
 
 double& PolynomialMap::coff(int i) {
-	// TODO
-	static double ERROR; // you should delete this line
-	return ERROR; // you should return a correct value
+	return m_Polynomial[i]; // default value is 0.f
 }
 
 void PolynomialMap::compress() {
-	// TODO
+	for (auto it = m_Polynomial.begin(); it != m_Polynomial.end();) {
+		if (std::abs(it->second) < 1e-8) {
+			it = m_Polynomial.erase(it);
+		} else {
+			++it;
+		}
+	}
 }
 
 PolynomialMap PolynomialMap::operator+(const PolynomialMap& right) const {
-	// TODO
-	return {}; // you should return a correct value
+	PolynomialMap result(*this);
+	for (const auto& term : right.m_Polynomial) {
+		result.m_Polynomial[term.first] += term.second;
+	}
+	result.compress();
+	return result;
 }
 
 PolynomialMap PolynomialMap::operator-(const PolynomialMap& right) const {
-	// TODO
-	return {}; // you should return a correct value
+	PolynomialMap result(*this);
+	for (const auto& term : right.m_Polynomial) {
+		result.m_Polynomial[term.first] -= term.second;
+	}
+	result.compress();
+	return result;
 }
 
 PolynomialMap PolynomialMap::operator*(const PolynomialMap& right) const {
-	// TODO
-	return {}; // you should return a correct value
+	PolynomialMap result;
+	for (const auto& term1 : m_Polynomial) {
+		for (const auto& term2 : right.m_Polynomial) {
+			int deg = term1.first + term2.first;
+			double cof = term1.second * term2.second;
+			result.m_Polynomial[deg] += cof;
+		}
+	}
+	result.compress();
+	return result;
 }
 
 PolynomialMap& PolynomialMap::operator=(const PolynomialMap& right) {
-	// TODO
+	m_Polynomial = right.m_Polynomial;
 	return *this;
 }
 
 void PolynomialMap::Print() const {
-	// TODO
+	// Special: empty
+	if (m_Polynomial.empty()) {
+		cout << "0" << endl;
+		return;
+	}
+	for (auto& term = m_Polynomial.rbegin(); term != m_Polynomial.rend(); ++term) {
+		cout << term->second << "x^" << term->first << " ";
+	}
+	cout << endl;
 }
 
 bool PolynomialMap::ReadFromFile(const string& file) {
     m_Polynomial.clear();
-	// TODO
-	return false; // you should return a correct value
+	ifstream fin(file);
+	if (!fin.is_open()) {
+		cout << "Failed to open file: " << file << endl;
+		return false;
+	}
+	int deg;
+	double cof;
+	int num = 0;
+	char check;
+	if (!(fin >> check >> num) || check != 'P') {
+		cout << "Invalid file format: " << file << endl;
+		return false;
+	}
+	for (int i = 0; i < num; ++i) {
+		fin >> deg >> cof;
+		m_Polynomial[deg] += cof;
+	}
+
+	return true; // you should return a correct value
 }
